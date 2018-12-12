5e89eaa0373382531d139237bd66728a10b7e843
Janine Luk <janineluk95@gmail.com>
2018-12-12 13:28:58 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,26 @@
+  class PigLatinizer
+  
+  attr_accessor :word
+  
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def convert(word)
+    # get first consonant group: either two letters or one
+    if word.
+    # smile ==> ile + sm + ay
+    # pig => ig + p + ay
+    # eat => eat + way
+    # item => item + way
+    # then add ay or way
+  end
+  
+  def vowels?
+      word_array = word.split('')
+      word_array.each do |w|
+        if w == []
+  end
+  
+end
