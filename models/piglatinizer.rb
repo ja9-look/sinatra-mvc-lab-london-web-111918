dd62d2a1d3ddb0f5098df26b4eed19cb36af5d3f
Janine Luk <janineluk95@gmail.com>
2018-12-12 13:35:51 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+  class PigLatinizer
+  
+  attr_accessor :word
+  
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def convert(word)
+    # get first consonant group: either two letters or one
+    if word.
+    # smile ==> ile + sm + ay
+    # pig => ig + p + ay
+    # eat => eat + way
+    # item => item + way
+    # then add ay or way
+  end
+  
+  def vowels?
+      word_array = word.split('')
+      vowels = ["a","e","i","o","u"]
+      word_array.each do |w|
+        if vowels.include?(w.downcase)
+          
+  end
+  
+end
